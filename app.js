4abe85d8330886489f74087b6ebde9dc49c683b0
Manuel <mninahuanca4@gmail.com>
2021-04-10 21:02:29 -0400

    importando y usando fileSystem

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+const fs = require('fs');
+
 console.clear();
 console.log("======================");
 console.log("     Tabla del 5");
 console.log("======================");
 
-const num = 5;
+const num = 3;
+let salida = "";
 
 for (let i = 1; i <= 10; i++) {
-    console.log(`${num} x ${i} = ${num*i}`)
+    salida = salida + `${num} x ${i} = ${num * i}\n`;
 }
+
+console.log(salida);
+
+fs.writeFile(`tabla-${num}.txt`, salida, (err) => {
+    if (err) throw err;
+    console.log(`tabla-${num}.txt creado con exito`);
+} )
