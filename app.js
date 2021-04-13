cd742dd5f79885b57d6b39bf002787f45c7dd4ff
Manuel <mninahuanca4@gmail.com>
2021-04-13 19:03:14 -0400

    Recibir informacion desde linea de comandos

--- app.js
+++ app.js
@@ -2,8 +2,14 @@
 const { crearArchivo } = require('./helpers/multiplicar');
 console.clear();
 
+console.log(process.argv);
+const [, , arg3 = 'base=5'] = process.argv;
+const [,base=5] = arg3.split('=');
 
-const base = 6;
+console.log(base)
+
+
+// const base = 6;
 crearArchivo(base)
     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
     .catch(err => console.log(err));
