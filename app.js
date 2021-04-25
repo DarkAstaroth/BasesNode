79e41045519888b301bdf3a4d1b0dbbc62df4e3d
Manuel <mninahuanca4@gmail.com>
2021-04-25 16:59:44 -0400

    Agregando un package.json

--- app.js
+++ app.js
@@ -2,11 +2,8 @@
 const { crearArchivo } = require('./helpers/multiplicar');
 console.clear();
 
-console.log(process.argv);
 const [, , arg3 = 'base=5'] = process.argv;
 const [,base=5] = arg3.split('=');
-
-console.log(base)
 
 
 // const base = 6;
